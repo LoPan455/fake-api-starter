b2704d82cf023d39d096e9fc95ef27f58dde61fd
lopan <tjohander@gmail.com>
2021-08-11 21:00:17 -0500

    Configured RestTemplate for JsonPlaceholder API client, implemented command line runner

--- src/main/kotlin/io/tjohander/fakeapistarter/FakeApiStarterApplication.kt
+++ src/main/kotlin/io/tjohander/fakeapistarter/FakeApiStarterApplication.kt
@@ -1,11 +1,27 @@
 package io.tjohander.fakeapistarter
 
+import io.tjohander.fakeapistarter.model.Post
+import io.tjohander.fakeapistarter.service.PostService
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class FakeApiStarterApplication
+class FakeApiStarterApplication(
+    @Autowired private val postService: PostService
+) : CommandLineRunner {
+
+    private val log = LoggerFactory.getLogger(FakeApiStarterApplication::class.java)
+
+    override fun run(vararg args: String?) {
+        log.info("Starting 'run'...")
+        val posts: List<Post>? = postService.getPosts()
+        posts?.map { log.info(it.toString()) }
+    }
+}
 
 fun main(args: Array<String>) {
-    runApplication<FakeApiStarterApplication>(*args)
+    SpringApplication.run(FakeApiStarterApplication::class.java, *args)
 }
